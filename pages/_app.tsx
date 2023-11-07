b8beb66654c108abd49ef93d1d7eb22e5cfe09c6
cinos81 <cinos81@gmail.com>
2023-11-07 21:25:59 +0900

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
